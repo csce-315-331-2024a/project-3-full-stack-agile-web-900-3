d1e925553e06861c1a44d012305d4d33d563178e
Quinn Bromley <quinnbromley52@tamu.edu>
2024-03-29 16:44:02 -0500

    this should work i think

--- demo/src/main/java/projectone/demo/DemoApplication.java
+++ demo/src/main/java/projectone/demo/DemoApplication.java
@@ -2,22 +2,24 @@ package projectone.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@RestController
+
 
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping("/")
-  public String hello() {
-    return "Hello poop";
-  }
+	@RequestMapping("/")
+        public String index() {
+            return "index.html";
+        }
 
 
 }
---
+++ demo/src/main/java/projectone/demo/HomeController.java
@@ -0,0 +1,17 @@
+package projectone.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+public class HomeController {
+    
+    @RequestMapping("/start")
+    public String start()
+    {
+        return "index";
+    }
+
+}
