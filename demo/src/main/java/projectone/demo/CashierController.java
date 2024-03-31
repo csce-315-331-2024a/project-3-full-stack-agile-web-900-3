f7e0c7ddb81efa9f587d5673993f71fccafbe40e
Kaghan Odom <odom.kaghan@tamu.edu>
2024-03-30 20:44:03 -0500

    Add updated HTML and Java files

---
+++ demo/src/main/java/projectone/demo/CashierController.java
@@ -0,0 +1,22 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class CashierController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // Return the name of your HTML file (index.html)
+    }
+
+    @PostMapping("/updateText")
+    @ResponseBody
+    public String updateText(@RequestParam String newText) {
+        // Handle logic to update variables based on newText
+        return newText;
+    }
+}
