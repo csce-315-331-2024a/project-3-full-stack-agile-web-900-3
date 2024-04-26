695ff5fe4b177148641fe94d2e78f8a022d0095e
Quinn Bromley <quinnbromley52@tamu.edu>
2024-04-25 19:31:09 -0500

    commit before merge main

--- demo/src/main/java/projectone/demo/DemoApplication.java
+++ demo/src/main/java/projectone/demo/DemoApplication.java
@@ -20,11 +20,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 
 
-		MenuBoard test = new MenuBoard();
-		test.categoryMap.forEach((key, value) -> {
-			//System.out.println("Category: " + key);
-			//value.forEach(System.out::println);
-		});
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
