c40de345207cf89c0b068efbd243510b3f0faa63
Kaghan Odom <odom.kaghan@tamu.edu>
2024-04-25 19:18:10 -0500

    temp

--- demo/src/main/java/projectone/demo/model/Inventory.java
+++ demo/src/main/java/projectone/demo/model/Inventory.java
@@ -20,7 +20,7 @@ import jakarta.persistence.Column;
 public class Inventory implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "name", nullable = false)
